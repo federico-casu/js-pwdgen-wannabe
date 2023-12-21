6e9f78a810a8f711783f2fc6df873de765c2bacd
federico-casu <federico.casu1101@gmail.com>
2023-12-21 16:47:34 +0100

    feat(main.js): added some other version + bonus version

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-document.writeln("<h1><em>" + prompt('Inserisci il tuo nome:', 'Federico') + prompt('Inserisci il tuo cognome:', 'Casu') + prompt('Inserisci il tuo colore preferito:', 'Giallo') + 23 + "</em></h1>");
+// document.writeln(
+//     `
+//     <h1>
+//     <em>${prompt('Inserisci il tuo nome:', 'Federico')}${prompt('Inserisci il tuo cognome:', 'Casu')}${prompt('Inserisci il tuo colore preferito:', 'Giallo')}23</em>
+//     </h1>
+//     `
+// );
+
+// alternative version 1
+// document.writeln("<h1><em>" + prompt('Inserisci il tuo nome:', 'Federico') + prompt('Inserisci il tuo cognome:', 'Casu') + prompt('Inserisci il tuo colore preferito:', 'Giallo') + 23 + "</em></h1>");
 
 
-
-// alternative version
+// alternative version 2
 /*
 let nome = prompt('Inserisci il tuo nome:', 'Federico');
 let cognome = prompt('Inserisci il tuo cognome:', 'Casu');
@@ -11,3 +19,24 @@ const finalNumber = 23;
 
 document.writeln('<h1><em>' + nome + cognome + colorePreferito + finalNumber + "</em></h1>");
 */
+
+/* --------------------
+    BONUS VERSION
+--------------------- */
+
+let formData = document.getElementById("passwordGeneratorForm");
+
+formData.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let nome = document.getElementById("name");
+    let cognome = document.getElementById("surname");
+    let colorePreferito = document.getElementById("favColor");
+
+    document.writeln(
+        `
+        La password generata è: ${nome.value}${cognome.value}${colorePreferito.value}23
+        `
+    );
+});
+
