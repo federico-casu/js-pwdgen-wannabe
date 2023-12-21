4e3e07fdb3f1af9ecaed076422464f667de974e0
federico-casu <federico.casu1101@gmail.com>
2023-12-21 13:55:17 +0100

    feat(main.js): password generator

---
+++ main.js
@@ -0,0 +1,13 @@
+document.writeln("<h1><em>" + prompt('Inserisci il tuo nome:', 'Federico') + prompt('Inserisci il tuo cognome:', 'Casu') + prompt('Inserisci il tuo colore preferito:', 'Giallo') + 23 + "</em></h1>");
+
+
+
+// alternative version
+/*
+let nome = prompt('Inserisci il tuo nome:', 'Federico');
+let cognome = prompt('Inserisci il tuo cognome:', 'Casu');
+let colorePreferito = prompt('Inserisci il tuo colore preferito:', 'Giallo');
+const finalNumber = 23;
+
+document.writeln('<h1><em>' + nome + cognome + colorePreferito + finalNumber + "</em></h1>");
+*/
